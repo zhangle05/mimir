0561606d518f69f68094c2bac6b4eb7e5920b1d9
Zhang Le <zhangle05@gmail.com>
2015-07-15 09:29:43 +0800

    Fix the bug of English map display is blank.

--- com.jotunheim.mimir.web/src/main/webapp/js/map-bing.js
+++ com.jotunheim.mimir.web/src/main/webapp/js/map-bing.js
@@ -2,9 +2,9 @@ function showMap(mapDivId, longitude, latitude, city) {
 	var map = new Microsoft.Maps.Map(document.getElementById(mapDivId), 
         {
             credentials:"AgRfLDspGNHAeYlZVFZtk5tBpVzYayNZTpmCM8JwVh6CAZDQfPqaIJwr-MIi-xdd",
-            center: new Microsoft.Maps.Location(longitude, latitude),
+            center: new Microsoft.Maps.Location(latitude, longitude),
             mapTypeId: Microsoft.Maps.MapTypeId.road,
-            zoom:7
+            zoom:13
         }
     );
 }
